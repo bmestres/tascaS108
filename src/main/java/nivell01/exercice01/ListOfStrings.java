6b01a00f9e32c1b46a303a969ba16388288869c5
Bernat Mestres <ber.mestres@gmail.com>
2026-07-04 16:47:53 +0200

    chore: initial commit

--- src/main/java/nivell01/exercice01/ListOfStrings.java
+++ src/main/java/nivell01/exercice01/ListOfStrings.java
@@ -1,11 +1,16 @@
 package nivell01.exercice01;
 
 import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
 
 public class ListOfStrings {
     public static final List<String> STRING_LIST = List.of("The world to come", "Moon", "Zama", "We are the best", "Man on wire", "Talk");
 
     public List<String> filterElementsWithLetterO() {
-        return STRING_LIST.stream().filter((string) -> string.contains("o") || string.contains("O")).toList();
+
+        Predicate<String> stringFilter = (string)-> string.contains("o") || string.contains("O");
+        return STRING_LIST.stream().filter(stringFilter).toList();
     }
 }
--- src/main/java/nivell01/exercice02/ListOfStrings.java
+++ src/main/java/nivell01/exercice02/ListOfStrings.java
@@ -1,12 +1,15 @@
 package nivell01.exercice02;
 
 import java.util.List;
+import java.util.function.Predicate;
 
+import static java.util.stream.Collectors.toList;
 import static nivell01.exercice01.ListOfStrings.STRING_LIST;
 
 public class ListOfStrings {
 
     public List<String> filterElementsWithLetterOAndLargerThanFive() {
-        return STRING_LIST.stream().filter((string) -> (string.contains("o") || string.contains("O")) && string.length() > 5).toList();
+        Predicate<String> stringFilter = (string) -> ((string.contains("o") || string.contains("O")) && string.length() > 5);
+        return STRING_LIST.stream().filter(stringFilter).toList();
     }
 }
