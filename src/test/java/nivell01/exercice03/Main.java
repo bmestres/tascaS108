086ce784889f488e0e4bbb3788349b604ea6b2c5
Bernat Mestres <ber.mestres@gmail.com>
2026-07-08 13:22:01 +0200

    feat: implemented Numbers interface to perform different basic operations

---
+++ src/test/java/nivell01/exercice03/Main.java
@@ -0,0 +1,12 @@
+package nivell01.exercice03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Numbers sum = (number1, number2) -> {return number1 + number2;};
+        Numbers subtraction = (number1, number2) -> {return number1 - number2;};
+        Numbers product = (number1, number2) -> {return number1*number2;};
+        Numbers division =(number1, number2) -> {return number1/number2;};
+
+    }
+}
---
+++ src/test/java/nivell01/exercice03/Numbers.java
@@ -0,0 +1,5 @@
+package nivell01.exercice03;
+@FunctionalInterface
+public interface Numbers {
+    float operation(float number1, float number2);
+}
