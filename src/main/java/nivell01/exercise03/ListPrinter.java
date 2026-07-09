b73d1d157c70eee194579485932fd8c47aaed414
Bernat Mestres <ber.mestres@gmail.com>
2026-07-09 23:10:01 +0200

    fix: adjusted printElements method

--- src/main/java/nivell01/exercise03/ListPrinter.java
+++ src/main/java/nivell01/exercise03/ListPrinter.java
@@ -9,6 +9,6 @@ public class ListPrinter {
     }
     public static void printElements(List<String> list){
         Consumer<String> display = (month) -> {System.out.format("%s\n", month);};
-        list.forEach(display);
+        list.stream().forEach(display);
     }
 }
